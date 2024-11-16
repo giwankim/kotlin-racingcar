cbcefe8de4cb735ba93dbc93dea14ba0fcd574d5
Giwan Kim <giwan.kim@gmail.com>
2024-11-16 12:49:37 +0900

    refactor: 연산자 enum 클래스를 람다 표현식을 생성자 인자로 활용하여 단순화 하였다.

--- src/main/kotlin/calculator/Operator.kt
+++ src/main/kotlin/calculator/Operator.kt
@@ -2,36 +2,17 @@ package calculator
 
 enum class Operator(
     val symbol: String,
+    val operation: (Int, Int) -> Int,
 ) {
-    ADD("+") {
-        override fun apply(
-            x: Int,
-            y: Int,
-        ): Int = x + y
-    },
-    SUBTRACT("-") {
-        override fun apply(
-            x: Int,
-            y: Int,
-        ): Int = x - y
-    },
-    MULTIPLY("*") {
-        override fun apply(
-            x: Int,
-            y: Int,
-        ): Int = x * y
-    },
-    DIVIDE("/") {
-        override fun apply(
-            x: Int,
-            y: Int,
-        ): Int = if (y != 0) x / y else throw IllegalArgumentException("0으로 나눌 수 없습니다.")
-    }, ;
+    ADD("+", { x, y -> x + y }),
+    SUBTRACT("-", { x, y -> x - y }),
+    MULTIPLY("*", { x, y -> x * y }),
+    DIVIDE("/", { x, y -> if (y != 0) x / y else throw IllegalArgumentException("0으로 나눌 수 없습니다.") }), ;
 
-    abstract fun apply(
+    fun apply(
         x: Int,
         y: Int,
-    ): Int
+    ): Int = operation(x, y)
 
     companion object {
         fun fromSymbol(symbol: String): Operator =
