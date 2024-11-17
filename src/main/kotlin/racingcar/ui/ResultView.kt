133cac22da95f2a7b62e5c3dcfac3a4786fd5025
Giwan Kim <giwan.kim@gmail.com>
2024-11-17 19:29:29 +0900

    feat: StdIn과 StdOut을 사용하는 UI 아웃풋 로직을 구현했다.

---
+++ src/main/kotlin/racingcar/ui/ResultView.kt
@@ -0,0 +1,21 @@
+package racingcar.ui
+
+import racingcar.application.GameResult
+import racingcar.application.RoundResult
+
+private const val RESULTS_HEADER = "\n실행 결과"
+private const val POSITION_DELIMITER = "-"
+
+fun printResult(result: GameResult) {
+    println(RESULTS_HEADER)
+    result.resultsByRound.forEach(::printRound)
+}
+
+private fun printRound(roundResult: RoundResult) {
+    roundResult.positions.forEach(::printPosition)
+    println()
+}
+
+private fun printPosition(position: Int) {
+    println(POSITION_DELIMITER.repeat(position))
+}
